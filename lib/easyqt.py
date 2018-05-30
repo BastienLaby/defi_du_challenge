927f7efce04d3889b500cf26b1e7a2cfe8d91c5e
bastien.laby <bastien.laby@teamto.com>
2018-05-30 20:45:29 +0200

    First Commit

---
+++ lib/easyqt.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+# @package easyqt
+# created on 10/08/2016 by bastien.laby
+# last updated on 29/12/2016
+
+import sys
+import os
+import platform
+
+try:
+    from core.ttLogger import ttLogger
+except ImportError:
+    sys.path.append('//srv-bin/bin/python/teamto')
+    from core.ttLogger import ttLogger
+log = ttLogger.createTTLogger()
+
+if platform.architecture()[0] == '64bit':
+    sys.path.append('//srv-bin/bin/python/modules27_64')
+else:
+    sys.path.append('//srv-bin/bin/python/modules27')
+try:
+    from PySide import QtCore, QtGui
+    from PySide.QtGui import QPalette, QColor
+    from PySide.QtCore import Qt
+except ImportError:
+    log.error('No module PySide found.')
+    raise
+
+
+class EasyBase(object):
+    '''
+    '''
+
+    def __init__(self, **signals):
+        self.signals = signals
+        for signalName, callback in signals.iteritems():
+            if signalName == 'objectName':
+                self.setObjectName(callback)
+                continue
+            try:
+                getattr(self, signalName).connect(callback)
+            except AttributeError:
+                log.warning('Signal %s is not a builtin signal for the type %s', signalName, type(self))
+                raise
+
+
+class EasyApplication(QtGui.QApplication):
+    '''
+    '''
+
+    def __init__(self):
+        super(EasyApplication, self).__init__(sys.argv)
+
+
+class EasyWindow(QtGui.QMainWindow, EasyBase):
+    '''
+    '''
+
+    def __init__(self, show=True, title='EasyQT Window', **signals):
+        super(EasyWindow, self).__init__(**signals)
+        if show:
+            self.show()
+        self.setWindowTitle(title)
+
+
+class EasyWidget(QtGui.QWidget, EasyBase):
+    '''
+    '''
+
+    def __init__(self, layoutType=None, layout='VBoxLayout', **signals):
+        super(EasyWidget, self).__init__(**signals)
+        if layoutType:
+            log.warning('EasyWidget : layoutType parameter is deprecated. Use "layout" parameter instead')
+            self.layout = layoutType()
+        if layout == 'VBoxLayout':
+            self.layout = QtGui.QVBoxLayout()
+        elif layout == 'HBoxLayout':
+            self.layout = QtGui.QHBoxLayout()
+        elif layout == 'GridLayout':
+            self.layout = QtGui.QGridLayout()
+        else:
+            raise Exception('EasyWidget : Unknow layout %s' % layout)
+        self.setLayout(self.layout)
+
+    def addWidget(self, widget, row=-1, col=-1, rowSpan=1, colSpan=1, stretch=0):
+        '''
+        Add the given widget to the current layout.
+        If the widget layout is a QGridLayout, the parameters "row" and "col" must be passed.
+        '''
+
+        if isinstance(widget, str):
+            widget = QtGui.QLabel(widget)
+
+        if isinstance(self.layout, QtGui.QGridLayout):
+            self.layout.addWidget(widget, row, col, rowSpan, colSpan, stretch=stretch)
+        else:
+            print widget
+            self.layout.addWidget(widget)
+
+    def addWidgets(self, *widgets):
+        '''
+        Add several widgets to the current layout.
+        QGridLayout is not supported for this function.
+        '''
+        if isinstance(self.layout, QtGui.QGridLayout):
+            raise Exception('GridLayout is not supported with addWidgets(*widgets) function')
+        print widgets
+        for widget in widgets:
+            self.addWidget(widget)
+
+
+class EasyPushButton(QtGui.QPushButton, EasyBase):
+    '''
+    A proxy class used to simplify the widget/layout management.
+    '''
+    def __init__(self, text, **signals):
+        '''
+        '''
+        super(EasyPushButton, self).__init__(**signals)
+        self.setText(text)
+
+
+class EasyLabel(QtGui.QLabel, EasyBase):
+    '''
+    A proxy class used to simplify the widget/layout management.
+    '''
+    def __init__(self, text, **signals):
+        '''
+        '''
+        super(EasyLabel, self).__init__(**signals)
+        self.setText(text)
+
+
+
+def callback():
+    print 'callback'
+
+
+if __name__ == '__main__':
+    app = EasyApplication()
+    window = EasyWindow()
+
+    centralWidget = EasyWidget(layout='GridLayout')
+    window.setCentralWidget(centralWidget)
+
+    vWidget = EasyWidget(layout='VBoxLayout')
+    centralWidget.addWidget(vWidget, 0, 0)
+    for i in range(5):
+        vWidget.addWidget(EasyPushButton(str(i), clicked=callback))
+
+    hWidget = EasyWidget(layout='HBoxLayout')
+    centralWidget.addWidget(hWidget, 0, 1)
+    hWidget.addWidgets(*[EasyPushButton(str(i), clicked=callback) for i in range(3)])
+
+
+    sys.exit(app.exec_())
---
+++ main.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import random
+import time
+
+import lib.easyqt as eqt
+from PySide import QtCore
+
+
+class Game(object):
+    def __init__(self, name):
+        self.name = name
+        self.revealed = False
+
+    def getDisplay(self):
+        if self.revealed:
+            return self.name
+        return ''.join(['_ ' if i != ' ' else ' ' for i in self.name])
+
+
+class DefiDuChallengeWindow(eqt.EasyWindow):
+
+    # unit = secs
+    initialBillboardDisplayTime = 0.1
+    revelationTime = 2
+
+    def __init__(self):
+        super(DefiDuChallengeWindow, self).__init__()
+        self.setWindowTitle('Defi du challenge')
+
+        self.games = []
+        self.games.append(Game('MARIO KART DOUBLE DASH'))
+        self.games.append(Game('GENITAL JOUSTING'))
+        self.games.append(Game('BASKHEAD'))
+        self.currentGameIndex = 0
+
+        self.widget = eqt.EasyWidget()
+
+        self.displayText = eqt.EasyLabel('DEFI DU CHALLENGE !')
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.displayGame)
+
+        self.goButton = eqt.EasyPushButton('GO', clicked=self.revealNextGame)
+        self.resetButton = eqt.EasyPushButton('RESET', clicked=self.reset)
+
+        self.widget.addWidgets(self.displayText, self.goButton, self.resetButton)
+
+        self.setCentralWidget(self.widget)
+
+        self.show()
+
+    def displayGame(self):
+
+        if time.time() - self.displayStartTime > 3 and not self.games[self.currentGameIndex].revealed:
+
+            self.games[self.currentGameIndex].revealed = True
+            self.displayText.setText('>>' + self.games[self.currentGameIndex].getDisplay() + ' !!')
+            self.timer.stop()
+            self.goButton.setEnabled(any(not i.revealed for i in self.games))
+
+        else:
+
+            self.displayText.setText(self.games[self.currentGameIndex].getDisplay())
+            self.currentGameIndex = (self.currentGameIndex + 1) % len(self.games)
+            self.timer.setInterval(self.timer.interval() * 1.1)
+
+
+    def revealNextGame(self):
+        self.displayStartTime = time.time()
+        self.currentGameIndex = random.randint(0, len(self.games) - 1)
+        self.goButton.setEnabled(False)
+        self.timer.start(500)
+
+    def reset(self):
+        for i in self.games:
+            i.revealed = False
+        self.goButton.setEnabled(True)
+
+
+
+if __name__ == '__main__':
+    app = eqt.EasyApplication()
+    window = DefiDuChallengeWindow()
+    app.exec_()
+
+
+
